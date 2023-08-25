bba8279b26005402fba08c8a9df5619a94ca0fc9
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-08-25 16:38:57 -0400

    Implementation of login form.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import App from './App';
+import './index.css';
+import * as sessionActions from "./store/session";
+
+import configureStore from './store';
+
+const store = configureStore();
+
+
+
+
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
